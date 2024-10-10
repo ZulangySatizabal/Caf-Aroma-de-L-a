07597b92f8c21de93a9bc85bdb60a53baf818450
Zulangy Satizabal <zulangy.sati15@gmail.com>
2024-10-10 16:57:09 -0500

    responsive navbar

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import { renderProducts } from "./modules/products.js";
-import { toggleMenu } from "./modules/toggleMenu.js";
+import { dropdownMenu, toggleMenu } from "./modules/btnActive.js";
 
 // función para que se rendericen las tarjetas
 renderProducts();
 //mostrar u ocultar el nav responsive
 toggleMenu();
+//mostrar u ocultar el submenu
+dropdownMenu();
---
+++ modules/btnActive.js
@@ -0,0 +1,25 @@
+/* Alterna la visibilidad del menú de navegación cuando se hace clic en el botón de menú. */
+
+export function toggleMenu(){
+    let button = document.querySelector("#menu-toggle-button");
+    let navMenu = document.getElementById("nav-menu");
+
+    button.addEventListener("click", function() {
+        navMenu.classList.toggle("active");
+    });
+};
+
+
+/* Alterna la visibilidad del submenu cuando se hace clic en la opción Productos */
+
+export function dropdownMenu(){
+    const dropdown = document.querySelectorAll('.dropdown');
+
+    dropdown.forEach(dropdown => {
+        dropdown.addEventListener('click', () => {
+            console.log("click")
+            dropdown.classList.toggle('active');
+        });
+    });
+
+}
